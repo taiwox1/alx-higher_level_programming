38d9e8ef49df7862615b71be6f50f6014407f226
Adedotun Taiwo <taiwoadedotunx1@gmail.com>
2022-06-29 01:51:15 +0100

    output is a random function

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+
 import random
-number = random.randint(-10000, 10000)
+number = random.randint(-100, 100)
 if number > 0:
     print(number,'is positive')
 elif number < 0:
-    print(number, 'is negative')
+    print(number,'is negative')
 else:
-    print(number, 'is zero')
+    print(number,'is zero')
 
