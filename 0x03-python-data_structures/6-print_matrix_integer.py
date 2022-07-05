805891cd38e6b21aeb1545544e456f0328ece34b
Adedotun Taiwo <taiwoadedotunx1@gmail.com>
2022-07-05 04:30:02 +0100

    what does the matrix hold, print it

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in range(len(i)):
-            print("{:d}".format(i[j]), end ="" if j == len(i) - 1 else " " )
+            print("{:d}".format(i[j]), end="" if j == len(i) - 1 else " ")
         print("")
