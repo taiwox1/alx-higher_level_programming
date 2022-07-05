1fc402aaafca9bc5315de62203b20c1fb7d2819f
Adedotun Taiwo <taiwoadedotunx1@gmail.com>
2022-07-05 05:49:01 +0100

    Data structure project

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,4 +3,4 @@ def delete_at(my_list=[], idx=0):
 
     if my_list is not None and 0 <= idx < len(my_list):
         del my_list[idx]
-    return 
+    return my_list
--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 a = 89
 b = 10
-a_temp = b
-b_temp = a
-a, b = a_temp, b_temp
-
+a, b = b, a
 print("a={:d} - b={:d}".format(a, b))
