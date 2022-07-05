ebb54e1936d875185c2a0504aeeb62f3e5281334
Adedotun Taiwo <taiwoadedotunx1@gmail.com>
2022-07-05 14:30:29 +0100

    remove the things at that position

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if my_list is not None and 0 <= idx < len(my_list):
+    if  0 >= idx < len(my_list):
+        return my_list
+    else:
         del my_list[idx]
+        return my_list
