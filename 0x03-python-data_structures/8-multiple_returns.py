8e6766abfe68b8d4a11527b475fd9968303cc42d
Adedotun Taiwo <taiwoadedotunx1@gmail.com>
2022-07-05 05:01:10 +0100

    get me his hight and his initial

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,8 @@
 def multiple_returns(sentence):
     if len(sentence) <= 0:
         return None
-    Y = len(sentence)
-    X = sentence[0]
-    tup = Y, X
-    return(tup)
+    else:
+        Y = len(sentence)
+        X = sentence[0]
+        tup = Y, X
+        return(tup)
