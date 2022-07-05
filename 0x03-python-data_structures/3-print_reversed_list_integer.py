4c63d77eb249d3a7f73924983f740646739104cc
Adedotun Taiwo <taiwoadedotunx1@gmail.com>
2022-07-05 01:59:09 +0100

    function reverse a string

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/urs/bin/python3
-my_list = [2,3,4,5,6]
-new_list = []
-for i in range(len(my_list)):
-    new_list.append(my_list[len(my_list) - 1 - i])
-for i in new_list:
-    print("{:d}".format(i))
+def print_reversed_list_integer(my_list=[]):
+    new_list = []
+    for i in range(len(my_list)):
+        new_list.append(my_list[len(my_list) - 1 - i])
+    for i in new_list:
+        print("{:d}".format(i))
