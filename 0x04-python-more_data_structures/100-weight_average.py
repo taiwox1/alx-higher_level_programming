89a82071d768488bec7dcc4a6d8034c319d8e2e2
Adedotun Taiwo <taiwoadedotunx1@gmail.com>
2022-07-08 04:00:44 +0100

    data structure

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-import functools
 def weight_average(my_list=[]):
-    aveg = list(map(lambda x: (x[0] * x[1]), my_list))
-    all_deno = list(map(lambda x: x[1], my_list))
-    sum_averag = functools.reduce(lambda x,y: x+y, aveg)
-    sum_deno = functools.reduce(lambda x,y: x+y, all_deno)
-    result = sum_averag / sum_deno
-    return(result)
+    if len(my_list) == 0:
+        return 0
+    return sum([x*y for (x, y) in my_list]) / sum([y for (x, y) in my_list])
