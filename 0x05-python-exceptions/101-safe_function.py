ea6196a2aae7e08c5c32f52d2731c4647a883bef
Adedotun Taiwo <taiwoadedotunx1@gmail.com>
2022-07-19 00:29:19 +0100

    python Error Handling

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,6 +6,6 @@ def safe_function(fct, *args):
     try:
         result = fct(*args)
         return (result)
-    except (TypeError, ValueError, ZeroDivisionError, OSError, RuntimeError):
+    except (NameError,IndexError, TypeError, ValueError, ZeroDivisionError, OSError, RuntimeError):
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
