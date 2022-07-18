dbefa4ed509eb316d27aeaffad4d210b0c6a4fb7
Adedotun Taiwo <taiwoadedotunx1@gmail.com>
2022-07-18 23:34:59 +0100

    python Error handling

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,7 +6,6 @@ def safe_print_division(a, b):
         return(result)
     except ZeroDivisionError:
         result = None
-        return(result)
     finally:
         print("Inside result: {} ". format(result))
     return(result)
