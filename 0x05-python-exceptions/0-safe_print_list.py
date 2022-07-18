e4e8eecbdbda37a16c10f8d1e786c04f4f5f17f5
Adedotun Taiwo <taiwoadedotunx1@gmail.com>
2022-07-18 20:11:30 +0100

    python Error Handling

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for i in range(x):
+    num = 0
+    for i in range(x):
+        try:
             print("{}".format(my_list[i]), end ="")
-        print("\n")
-        return(x)
-    except IndexError:
-        n = 0;
-        print("\n")
-        for i in my_list:
-            n += 1;
-        return(n)
-    except TypeError:
-        print("TypeError")
+            num += 1
+        except IndexError:
+            break
+    print("")
+    return(num)
