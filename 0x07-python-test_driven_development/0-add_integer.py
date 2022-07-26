c4f94c5bee24839789f64d0260d7ff76b0f349fa
Adedotun Taiwo <taiwoadedotunx1@gmail.com>
2022-07-26 17:59:44 +0100

    Test driven development: writting a doc test

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# 0-add_integer.py
+"""Defines an integer addition function."""
+
+def add_integer(a, b=98):
+    """"Returns an intege: addition of a and b
+
+    Float are typecased to int before addition
+    
+    Raises:
+        TypeError: if either of a and b is a non-integer and non-float.
+    """
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return(int(a) + int(b))
