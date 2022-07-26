0859b120c9b1b1853acefdb0538d94854a8b1fa0
Adedotun Taiwo <taiwoadedotunx1@gmail.com>
2022-07-27 00:19:24 +0100

    python test driven development

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,20 @@
+#!/bin/usr/python3
+# 3-say_my_name.py
+
+"""Defining function ``say_last_name``"""
+
+
+def say_my_name(first_name, last_name=""):
+
+    """function Prints a name.
+    Argument:
+        first_name (str)
+        last_name (str)
+    Raises:
+        TypeError: If either of first_name or last_name are not strings.
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
