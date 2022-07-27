1de8ca6b03058ca8b73ecb6cb576817bd6fc8f4f
Adedotun Taiwo <taiwoadedotunx1@gmail.com>
2022-07-27 01:18:52 +0100

    python diriven development

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,23 @@
+#!/urs/bin/python3
+# 5-text_indentation.py
+
+"""Difining function"""
+
+
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    char_num = 0
+    while char_num < len(text) and text[c] == ' ':
+        char_num += 1
+    while char_num < len(text):
+        print(text[char_num], end="")
+        if text[char_num] == "\n" or text[char_num] in ".?:":
+            if text[char_num] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[char_num] == ' ':
+                char_num += 1
+            continue
+        c += 1
