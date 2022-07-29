e404007c563dd0368de6d4a0fe268f21100f9aca
Adedotun Taiwo <taiwoadedotunx1@gmail.com>
2022-07-29 11:06:37 +0100

    python exception

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,6 +6,6 @@ def safe_function(fct, *args):
     try:
         result = fct(*args)
         return (result)
-    except (IndexError, TypeError, ValueError, OSError, RuntimeError):
+    except:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
