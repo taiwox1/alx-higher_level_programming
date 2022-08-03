18eeb572157101bbb58e6a157e8cd40a8e823414
Adedotun Taiwo <taiwoadedotunx1@gmail.com>
2022-08-03 05:02:47 +0100

    python input output

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defines a class Student."""
+
+
+class Student:
+    """Represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
+        Args:
+            first_name (str):  student first name
+            last_name (str): student last name.
+            age (int):  student age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the Student.
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
+        Args:
+            attrs (list): attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
