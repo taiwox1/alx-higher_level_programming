39ce19bf9a36ed6311861be1866f76bab7693fe5
Adedotun Taiwo <taiwoadedotunx1@gmail.com>
2022-08-09 00:08:58 +0100

    almost circle python

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
 
+"""Defines a base model class"""
 
 class Base:
     """ This class will be the “base” of all other classes in this project"""
     """
         args: __nb_object(int): the numbers of instances in base
     """
-    __nb_object = 0
+    __nb_objects = 0
 
     def __init__(self, id=None):
+        """ base initailization 
+            Args:
+                id(int): identity of the new basei
+        """
+
         if id is not None:
             self.id = id
         else:
-            Base.__nb_object += 1
+            Base.__nb_objects += 1
             self.id = Base.__nb_objects
