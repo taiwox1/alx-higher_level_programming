519af2935e0b0eea4bad7a94fc6aeb556d17dd46
Adedotun Taiwo <taiwoadedotunx1@gmail.com>
2022-10-09 19:11:59 +0100

    list name starting wwith N:

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,7 @@ import MySQLdb
 if __name__ == "__main__":
     dbase = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = dbase.cursor()
-    cur.execute("SELECT * FROM states WHERE name REGEXP '^[N]'")
+    cur.execute("SELECT * FROM states WHERE name REGEXP '^[N]' ORDER BY id")
     query_table = cur.fetchall()
     for state in query_table:
         print(state)
