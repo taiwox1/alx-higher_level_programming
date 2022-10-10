affd79b9a0cbf2fa328a2c3627c12808ecfb0a71
Adedotun Taiwo <taiwoadedotunx1@gmail.com>
2022-10-10 21:36:01 +0100

    search for  matches the argument

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,9 @@ import MySQLdb
 if __name__ == "__main__":
     dbase = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = dbase.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT *\
+            FROM states\
+            ORDER BY id ASC")
     query_table = cur.fetchall()
     for state in query_table:
         if state[1][0] == 'N':
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,8 +7,10 @@
     script thakes 4 arguments: <mysql username>, <mysql password>,
     <database name> and <state name searched>
 """
+
 from sys import argv
 import MySQLdb
+
 if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     search_name = argv[4]
@@ -17,7 +19,5 @@ if __name__ == "__main__":
     states_guery = cur.fetchall()
     for state in states_guery:
         print(state)
-        """if state[1] == search_name:
-            print(state)"""
     cur.close()
     db.close()
