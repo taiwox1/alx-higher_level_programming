2dbcb57eff4c505e2cbbbf8ac7de0816d29c8573
Adedotun Taiwo <taiwoadedotunx1@gmail.com>
2022-10-10 22:22:44 +0100

    search for match

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -15,8 +15,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     search_name = argv[4]
     cur = db.cursor()
-    cur.execute("SELECT * \
-            FROM states \
+    cur.execute("SELECT *\
+            FROM states\
             WHERE BINARY name='{}'".format(argv[4]))
     states_guery = cur.fetchall()
     for state in states_guery:
