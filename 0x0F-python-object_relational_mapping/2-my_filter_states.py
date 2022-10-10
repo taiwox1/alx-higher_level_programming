3197750db1ed60faff94d441752808ace6112c82
Adedotun Taiwo <taiwoadedotunx1@gmail.com>
2022-10-10 21:30:40 +0100

    search for argument

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,10 +13,11 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     search_name = argv[4]
     cur = db.cursor()
-    cur.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'Arizona'")
     states_guery = cur.fetchall()
     for state in states_guery:
-        if state[1] == search_name:
-            print(state)
+        print(state)
+        """if state[1] == search_name:
+            print(state)"""
     cur.close()
     db.close()
