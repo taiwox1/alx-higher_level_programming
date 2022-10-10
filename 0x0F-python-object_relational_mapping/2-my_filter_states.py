97cefee2fcb3557b396522fca0bbf4c8ef68b064
Adedotun Taiwo <taiwoadedotunx1@gmail.com>
2022-10-10 21:40:05 +0100

    serch for match

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -15,7 +15,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     search_name = argv[4]
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'Arizona'")
+    cur.execute("SELECT *\
+            FROM states\
+            WHERE name LIKE 'Arizona'")
     states_guery = cur.fetchall()
     for state in states_guery:
         print(state)
