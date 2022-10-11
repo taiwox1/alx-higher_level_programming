42831c5b3db397f5112b6530a3a6e95af9b1d69b
Adedotun Taiwo <taiwoadedotunx1@gmail.com>
2022-10-11 06:50:06 +0100

    save frominjection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -13,10 +13,10 @@ from sys import argv
 
 if __name__ == "__main__":
     conn =  MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    nme = argv[4] 
     cur = conn.cursor()
-    cur.execute("SELECT * \
-            FROM `states` \
-            WHERE BINARY `name`='{}'".format(argv[4]))
+    cur.execute("SELECT * FROM states")
     states = cur.fetchall()
     for state in states:
-        print(state)
+        if state[1] == nme:
+            print(state)
