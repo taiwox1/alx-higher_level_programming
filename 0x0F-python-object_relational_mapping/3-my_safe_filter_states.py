22cbe0a2499463e67d3f59b508c37d6523732882
Adedotun Taiwo <taiwoadedotunx1@gmail.com>
2022-10-11 07:03:23 +0100

    save from injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -12,8 +12,8 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    conn =  MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
-    nme = argv[4] 
+    conn = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    nme = argv[4]
     cur = conn.cursor()
     cur.execute("SELECT * FROM states")
     states = cur.fetchall()
