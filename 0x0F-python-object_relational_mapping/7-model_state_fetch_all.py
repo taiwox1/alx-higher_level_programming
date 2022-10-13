f2723756eaaa9f8387a29bed30c88198848f9495
Adedotun Taiwo <taiwoadedotunx1@gmail.com>
2022-10-13 02:24:14 +0100

    lists all State objects from the database

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -17,10 +17,8 @@ if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
-
-Session = sessionmaker(bind=engine)
-session = Session()
-query_state = session.query(State).order_by(State.id).all()
-
-for state in query_state:
-    print("{}: {}".format(state.id, state.name))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query_state = session.query(State).order_by(State.id).all()
+    for state in query_state:
+        print("{}: {}".format(state.id, state.name))
