ba81ba8927334502b1f650dea88d904dae4dc6b6
Adedotun Taiwo <taiwoadedotunx1@gmail.com>
2022-10-14 23:35:01 +0100

    fetch city by state

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -12,7 +12,8 @@
 """
 import sqlalchemy
 from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
 
 
 class City(Base):
@@ -24,4 +25,4 @@ class City(Base):
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'))
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
