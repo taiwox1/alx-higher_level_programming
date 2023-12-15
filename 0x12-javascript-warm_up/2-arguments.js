317bb69733832c1a6929941086989223ce06eefb
taiwox1 <taiwoadedotunx1@gmail.com>
2023-12-15 20:59:20 +0100

    Script written to print a message depending on the number of arguments passed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-if (process.argv.length === 2) {
+if (process.argv[2] === null) {
   console.log('No argument');
-} else if (process.argv.length === 3) {
+} else if (process.argv[3] === null) {
   console.log('Argment found');
 } else {
   console.log('Arguments found');
