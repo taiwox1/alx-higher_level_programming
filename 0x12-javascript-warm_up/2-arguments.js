1ae3e46519e6619c6137822526986231186fb9e1
taiwox1 <taiwoadedotunx1@gmail.com>
2023-12-15 21:16:30 +0100

    print argumrnt based on number

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-if (process.argv[2] === null) {
+
+if (process.argv.length === 2) {
   console.log('No argument');
-} else if (process.argv[3] === null) {
+} else if (process.argv.length === 3) {
   console.log('Argment found');
 } else {
   console.log('Arguments found');
